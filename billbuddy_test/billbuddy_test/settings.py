16ea028816c05fee93ddd99c8fac174bfccb94dd
H09bdavies <h09bryndavies@gmail.com>
2019-07-07 14:49:33 +0100

    Added relational models and migrations

--- billbuddy_test/billbuddy_test/settings.py
+++ billbuddy_test/billbuddy_test/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shoestore.apps.ShoestoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- billbuddy_test/shoestore/models.py
+++ billbuddy_test/shoestore/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    brand_name = models.CharField(max_length=20)
+
+class Product(models.Model):
+  brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+  product_name = models.CharField(max_length=50)
+  product_desc = models.CharField(max_length=200)
+  pub_date = models.DateTimeField('date published')
+  price = models.DecimalField(max_digits=5, decimal_places=2)
+  stock_no = models.IntegerField()
+
